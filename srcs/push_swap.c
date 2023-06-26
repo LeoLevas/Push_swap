8a2bf0ec7df085333e41c92cd10f5070ca590197
llevasse <leonardlevasseur@yahoo.com>
2023-06-26 15:39:12 +0200

    added direct checking of presence of args in main.c to avoid useless malloc

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 16:50:17 by llevasse          #+#    #+#             */
-/*   Updated: 2023/06/26 09:43:24 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/06/26 15:34:25 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	main(int argc, char *argv[])
 	t_int_list	*lst;
 	t_int_list	*lst_simple;
 
+	if (argc == 1)
+		return (0);
 	lst = init_list(argc, argv);
 	lst_simple = init_list(argc, argv);
 	if (!lst || !lst_simple)
