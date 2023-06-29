468d9ee22857544a129653a09b13c61477e433da
llevasse <leonardlevasseur@yahoo.com>
2023-06-29 11:51:58 +0200

    fix unconditional jump

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 16:50:17 by llevasse          #+#    #+#             */
-/*   Updated: 2023/06/29 09:31:21 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/06/29 11:51:54 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	main(int argc, char *argv[])
 	lst_simple = NULL;
 	if (!lst)
 		return (kill_lst(lst, lst_simple), ft_printf("Error\n"));
-	if (is_stack_sorted(lst) || lst->len_a == 1)
+	if (lst->len_a == 1 || is_stack_sorted(lst))
 		return (kill_lst(lst, lst_simple), 0);
 	if (lst->len_a > 5)
 	{
